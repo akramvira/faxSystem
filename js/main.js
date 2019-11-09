d68bac0b247cdf1f404a47903ee026e0959631e5
akram <akram@jumbula.com>
2019-11-09 19:15:24 +0330

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+function createReciverRow(){
+    var a = 0;
+    var rcRow = `<div class="reciever-row">
+                    <input class="dst-number" /> 
+                    <input class="dst-ext" /> 
+                    <input class="time-delay" /> 
+                </div>
+                `;
+
+
+    //var parentDiv = jq()
+    //parentDiv.append(rcRow);
+}
+
+
+function submit(){
+    //jq('.reciever-row').each(function(){
+    // })
+}
