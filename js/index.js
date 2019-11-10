bde4a9d88c0cf11709676e66bd4cf9b9eac5d3a3
akram <akram@jumbula.com>
2019-11-11 00:27:26 +0330

    sending files ok

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 jq(function(){
+    updateFaxCount();
     setInterval(updateFaxCount, 10000);
     //setInterval(updateFaxesStatus, 3000);
 
@@ -16,6 +17,30 @@ function updateFaxCount(){
                 jq('#sent-count').text(data['send']);
                 jq('#archive-count').text(data['archive']);
                 jq('#all-count').text(data['all']);
+                
+                
+                jq('#sending-file .count').text(data['sending']['count']);
+
+                var sendingFiles = data['sending']['data'];
+
+                var sendingFileList = '';
+                for( var i = 0 ; i< sendingFiles.length ; i++){
+                    sendingFileList +=  `
+                    <h5 class="card-title file-title">- ${sendingFiles[i]['namefile']}</h5>
+                    <p class="card-text">
+                        <span class="file-name p-1">از: ${sendingFiles[i]['from']}</span>
+                        <span class="file-from p-1">به:${sendingFiles[i]['to']}</span>
+                        <span class="file-from p-1">ntries:${sendingFiles[i]['ntries']}</span>
+                        <span class="file-from p-1">state:${sendingFiles[i]['state']}</span>
+                        <span class="file-from p-1">file:${sendingFiles[i]['file']}</span>
+                        <span class="file-from p-1">تاریخ:${sendingFiles[i]['date']}</span>
+                    </p>
+                    `;
+                }
+
+                jq('#sending-file .files').html(sendingFileList);
+               
+
 
                 jq('#newFaxCount').text(data['data']);
             }
