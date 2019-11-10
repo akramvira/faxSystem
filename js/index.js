3ada8901ca4067dec7c37c18eb67f7c5c12384ec
ALiAsghar <tavakkoli.asr@gmail.com>
2019-11-10 22:32:58 +0330

    add new page $ change index file

--- js/index.js
+++ js/index.js
@@ -18,3 +18,21 @@ function updateFaxCount(){
 }
 
 function updateFaxesStatus(){}
+
+
+//pie
+var ctxP = document.getElementById("pieChart").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+    type: 'pie',
+    data: {
+        labels: ["بایگانی شده", "ارسال شده", "دریافت شده"],
+        datasets: [{
+            data: [100, 80, 100],
+            backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C"],
+            hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870"]
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
